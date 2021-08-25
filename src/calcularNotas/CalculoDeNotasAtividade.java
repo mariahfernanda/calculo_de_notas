7935cbdf11b3c8397b2fe6e7856b002576238421
Maria Fernanda <mariafernanda200505@gmail.com>
2021-08-25 16:33:53 -0300

    Versão 1 - 25-08

---
+++ src/calcularNotas/CalculoDeNotasAtividade.java
@@ -0,0 +1,22 @@
+package calcularNotas;
+
+public class CalculoDeNotasAtividade {
+
+	public static void main(String[] args) {
+		
+		double nota1Bim = 8.9;
+		double nota2Bim = 7.6;
+		double nota3Bim = 9.7;
+		double nota4Bim = 6.7;
+		
+		double resultadoFinal = (nota1Bim + nota2Bim + nota3Bim + nota4Bim)/4;
+		
+		System.out.print("A Aluna Média Fernanda ficou coom a média : ");
+		System.out.println(resultadoFinal); 
+		
+		
+				
+		
+	}
+
+}
